144be08370d7ba3d866b210694da64a0d10031f4
Paul Nechifor <paul@nechifor.net>
2017-03-02 15:22:19 -0500

    Convert to 64 bits.

--- elftpl/tiny.asm
+++ elftpl/tiny.asm
@@ -1,16 +1,15 @@
-BITS 32
-
+BITS 64
 org 0x08048000
 
-ehdr: ; Elf32_Ehdr
+ehdr: ; Elf64_Ehdr
   db 0x7F, "ELF", 1, 1, 1, 0 ; e_ident
   db 0, 0, 0, 0, 0, 0, 0, 0
   dw 2                       ; e_type
-  dw 3                       ; e_machine
+  dw 0x3E                    ; e_machine
   dd 1                       ; e_version
-  dd _start                  ; e_entry
-  dd phdr - ehdr             ; e_phoff
-  dd 0                       ; e_shoff
+  dq _start                  ; e_entry
+  dq phdr - ehdr             ; e_phoff
+  dq 0                       ; e_shoff
   dd 0                       ; e_flags
   dw phdr - ehdr             ; e_ehsize
   dw _start - phdr           ; e_phentsize
@@ -19,19 +18,19 @@ ehdr: ; Elf32_Ehdr
   dw 0                       ; e_shnum
   dw 0                       ; e_shstrndx
 
-phdr: ; Elf32_Phdr
+phdr: ; Elf64_Phdr
   dd 1           ; p_type
-  dd 0           ; p_offset
-  dd ehdr        ; p_vaddr
-  dd ehdr        ; p_paddr
-  dd _end - ehdr ; p_filesz
-  dd _end - ehdr ; p_memsz
   dd 5           ; p_flags
-  dd 0x1000      ; p_align
+  dq 0           ; p_offset
+  dq ehdr        ; p_vaddr
+  dq ehdr        ; p_paddr
+  dq _end - ehdr ; p_filesz
+  dq _end - ehdr ; p_memsz
+  dq 0x1000      ; p_align
 
 _start:
-  mov bl, 42
-  xor eax, eax
-  inc eax
-  int 0x80
+  mov rax, 60
+  mov rdi, 42
+  syscall
+
 _end:
